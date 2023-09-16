1a409088539abecde29782a1c460e91db7d00137
wanyun-yang-2002 <wanyun.yang@outlook.com>
2023-09-16 18:23:56 +0800

    modify files

--- Web框架_Gee/main.go
+++ Web框架_Gee/main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"gee"
-	"log"
+	"html/template"
 	"net/http"
 	"time"
 )
 
-func onlyForV2() gee.HandlerFunc {
-	return func(c *gee.Context) {
-		// start timer
-		t := time.Now()
-		// if a server error occured
-		c.Fail(500, "Internal Server Error")
-		// Calculate resolution time
-		log.Printf("[%d] %s in %v for group %v2", c.StatusCode, c.Req.RequestURI, time.Since(t))
-	}
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
 }
 
 func main() {
 	r := gee.New()
 	r.Use(gee.Logger()) // global middleware
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
 	})
+	r.LoadHTMLGlob("e:/computer/GO_Project/Web框架_Gee/templates/*")
+	r.Static("/assets", "./static")
 
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2()) // v2 group middleware
-	{
-		v2.GET("/hello/:name", func(c *gee.Context) {
-			// expect /hello/geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Lily", Age: 21}
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"strArr": [2]*student{stu1, stu2},
 		})
-	}
+	})
+	r.GET("/date", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Date(2023, 9, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
 
 	r.Run(":9999")
 }
--- Web框架_Gee/notes.md
+++ Web框架_Gee/notes.md
@@ -1030,6 +1030,67 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 ---main.go
 ``` 
 根据目录结构创建应有的文件夹及文件
+往`css.tmpl`中添加如下内容：
+```html
+<!-- day6-template/templates/css.tmpl -->
+<html>
+    <link rel="stylesheet" href="/assets/css/geektutu.css">
+    <p>geektutu.css is loaded</p>
+</html>
+``` 
+修改`main.go`
+```go
+package main
+
+import (
+	"fmt"
+	"gee"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func main() {
+	r := gee.New()
+	r.Use(gee.Logger()) // global middleware
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Lily", Age: 21}
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title": "gee"
+			"strArr": [2]*student{stu1, stu2},
+		})
+	})
+	r.GET("/date", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee"
+			"now":   time.Date(2023, 9, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
+	
+	r.Run(":9999")
+}
+``` 
+```go
+
+``` 
+```go
+
+``` 
 ```go
 
 ``` 
